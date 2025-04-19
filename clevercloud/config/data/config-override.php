0fb900b28eaafb338c5d29f8656baf0afdab7037
Jb Piacentino <jb@piacentino.com>
2025-04-19 15:57:01 +0200

    Refresh

--- clevercloud/config/data/config-override.php
+++
@@ -1,8 +0,0 @@
- <?php
- return [
-    'smtpServer' => getenv('SMTP_HOST'),
-    'smtpPort' => getenv('SMTP_PORT'),
-    'smtpAuth' => true,
-    'smtpSecurity' => 'TLS',
-    'smtpUsername' => getenv('SMTP_USER')
-];
